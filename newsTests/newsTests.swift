4e3b0ec55e669099a5ebae132c219e4c917a7955
Baris Dilekci <bdilekci200@gmail.com>
2025-05-12 14:22:47 +0300

    Initial Commit

---
+++ newsTests/newsTests.swift
@@ -0,0 +1,17 @@
+//
+//  newsTests.swift
+//  newsTests
+//
+//  Created by Barış Dilekçi on 12.05.2025.
+//
+
+import Testing
+@testable import news
+
+struct newsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
