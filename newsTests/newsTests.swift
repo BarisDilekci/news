b0aa04c5e78abeb738960483506abe76f6030d01
Baris Dilekci <bdilekci200@gmail.com>
2025-05-16 00:33:30 +0300

    feat: network structure tested with mock

--- newsTests/newsTests.swift
+++ newsTests/newsTests.swift
@@ -7,11 +7,58 @@
 
 import Testing
 @testable import news
+import Foundation
+
+final class MockNetworkService: NetworkServiceProtocol {
+    var result: Result<Data, Error>?
+    
+    func fetch<T>(endpoint: APIEndpoint) async throws -> T where T : Decodable {
+        guard let result = result else {
+            fatalError("Mock result is not set")
+        }
+        
+        switch result {
+        case .success(let data):
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                return decodedData
+            } catch {
+                throw NetworkError.decodingError
+            }
+        case .failure(let error):
+            throw error
+        }
+    }
+}
+
 
 struct newsTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+    private let networkService = NetworkService()
+    private let mockService = MockNetworkService()
 
+  
+    
+    @Test func test_request_success() async throws {
+        let jsonString = """
+          {
+              "title": "Apple WWDC 2025 Duyuruldu",
+              "description": "Apple, yıllık geliştirici konferansı WWDC'yi Haziran ayında düzenleyeceğini açıkladı.",
+              "imageURL": "https://via.placeholder.com/300x200.png?text=WWDC+2025"
+          }
+          """
+        
+        let data = jsonString.data(using: .utf8)
+        mockService.result = .success(data!)
+        
+        let endpoint = APIEndpoint.topHeadlines(country: "en", category: "technology")
+        
+        let news: News = try await mockService.fetch(endpoint: endpoint)
+        
+        #expect(news.title == "Apple WWDC 2025 Duyuruldu")
+        #expect(news.description == "Apple, yıllık geliştirici konferansı WWDC'yi Haziran ayında düzenleyeceğini açıkladı.")
+        #expect(news.imageURL ==  "https://via.placeholder.com/300x200.png?text=WWDC+2025")
+    }
+    
 }
+
